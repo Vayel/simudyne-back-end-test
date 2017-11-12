d130a0f7f6419d73d14ab9aa270cd10737252888
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-11-12 18:51:22 +0100

    Simulate all agents

--- simudyne/web.py
+++ simudyne/web.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, abort, request
 
 from . import model, simulation
+from .agent import BREED_C, BREED_NC
 
 app = Flask(__name__)
 model.connect('database.db')
@@ -46,3 +47,44 @@ def simulate_one(id_):
 
     states = simulation.simulate(agent, brand_factor, N_YEARS)
     return jsonify(states)
+
+
+@app.route('/simulate')
+def simulate_all():
+    try:
+        brand_factor = parse_brand_factor(request.args.get('brand_factor'))
+    except ValueError as e:
+        abort(500, str(e))
+
+    def create_year_resp():
+        return {'C': [], 'NC': [], 'C_lost': [], 'C_gained': [], 'C_regained': []}
+
+    resp = []
+    agents = list(model.get_all())
+
+    year_resp = create_year_resp()
+    year_resp['C'] = [agent.id_ for agent in agents if agent.breed == BREED_C]
+    year_resp['NC'] = [agent.id_ for agent in agents if agent.breed == BREED_NC]
+    resp.append(year_resp)
+
+    for _ in range(N_YEARS):
+        year_resp = create_year_resp()
+        for agent in agents:
+            original_breed = agent.breed
+            states = simulation.simulate(agent, brand_factor, 1)
+
+            if agent.breed == BREED_NC:
+                year_resp['NC'].append(agent.id_)
+
+                if original_breed == BREED_C:
+                    year_resp['C_lost'].append(agent.id_)
+            else:
+                year_resp['C'].append(agent.id_)
+
+                if original_breed == BREED_NC:
+                    year_resp['C_gained'].append(agent.id_)
+                elif BREED_NC in states.values():
+                    year_resp['C_regained'].append(agent.id_)
+        resp.append(year_resp)
+
+    return jsonify(resp)
