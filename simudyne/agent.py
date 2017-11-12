da7c864506ad995c4bfa64ea39a63923e0c437d7
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-11-12 17:51:29 +0100

    Add route to query agents by id

--- simudyne/agent.py
+++ simudyne/agent.py
@@ -20,6 +20,13 @@ class Agent:
         self.auto_renew = auto_renew
         self.inertia_for_switch = inertia_for_switch
 
+    def __str__(self):
+        return ('Agent(breed={breed}, id={id}, age={age}, social_grade={social_grade}, '
+                'payment_at_purchase={payment_at_purchase}, attribute_brand={attribute_brand}, '
+                'attribute_price={attribute_price}, attribute_promotions={attribute_promotions}, '
+                'auto_renew={auto_renew}, inertia_for_switch={inertia_for_switch})'
+                .format(**self.to_json()))
+
     def step(self, brand_factor):
         self.age += 1
         if self.auto_renew:
@@ -33,3 +40,17 @@ class Agent:
             self.breed = BREED_ENUM['NC']
         elif self.breed == BREED_ENUM['NC'] and affinity < self.social_grade * self.attribute_brand * brand_factor:
             self.breed = BREED_ENUM['C']
+
+    def to_json(self):
+        return dict(
+            breed=self.breed,
+            id=self.id_,
+            age=self.age,
+            social_grade=self.social_grade,
+            payment_at_purchase=self.payment_at_purchase,
+            attribute_brand=self.attribute_brand,
+            attribute_price=self.attribute_price,
+            attribute_promotions=self.attribute_promotions,
+            auto_renew=self.auto_renew,
+            inertia_for_switch=self.inertia_for_switch,
+        )
--- simudyne/web.py
+++ simudyne/web.py
@@ -1,6 +1,18 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify, abort
 
-@app.route("/")
-def hello():
+from . import model
+
+app = Flask(__name__)
+model.connect('database.db')
+
+@app.route('/')
+def home():
     return "Hello World!"
+
+
+@app.route('/agents/<id_>')
+def get_agent(id_):
+    agent = model.get_by_id(id_)
+    if agent is None:
+        abort(404)
+    return jsonify(**agent.to_json())
