68cab9d302eb198a1b3cb4a6ce118f499817f3bf
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-11-12 16:50:24 +0100

    Create web app structure

---
+++ run.py
@@ -0,0 +1,4 @@
+import simudyne
+
+if __name__ == '__main__':
+    simudyne.web.app.run()
--- setup.py
+++ setup.py
@@ -20,5 +20,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
+        'Flask',
     ],
 )
