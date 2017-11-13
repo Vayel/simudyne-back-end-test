b878bc7accf88c740237738ce3b63c95553b432d
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-11-13 23:34:52 +0100

    Render one-agent simulation

--- simudyne/static/js/home.js
+++ simudyne/static/js/home.js
@@ -7,8 +7,52 @@ $(document).ready(function() {
         $(form).find('.errors').append('<p>' + msg + '</p>');
     }
 
-    function renderOne(data) {
-        alert(data);
+    function renderOne(id, data) {
+        var points = [];
+        for(var date in data) {
+            points.push({x: date, y: data[date]});
+        }
+        console.log(points);
+
+        var ctx = document.getElementById('one_agent_chart').getContext('2d');
+        ctx.width = parseInt($('#one_agent_chart').attr('width'));
+        ctx.height= parseInt($('#one_agent_chart').attr('height'));
+
+        var chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: Object.keys(data),
+                datasets: [{
+                    label: 'Agent ' + id,
+                    steppedLine: true,
+                    data: Object.values(data),
+                    borderColor: 'red',
+					fill: false,
+                }]
+            },
+            options: {
+                maintainAspectRatio: false,
+                tooltips: false,
+                scales: {
+                    xAxes: [{
+                        offset: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Year'
+                        }
+                    }],
+                    yAxes: [{
+                        type: 'category',
+                        labels: ['C', 'NC'],
+                        offset: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Breed'
+                        }
+                    }]
+                }
+            }
+        });
     }
 
     $('#simulate_one_form').submit(function(e) {
@@ -20,9 +64,10 @@ $(document).ready(function() {
         var url = '/simulate/' + agentId + '?brand_factor=' + brandFactor;
         var form = this;
 
-        $.getJSON(url, renderOne)
-         .fail(function(jqXHR, textStatus, errorThrown) {
-             displayFormError(form, jqXHR.responseJSON);
-         });
+        $.getJSON(url, function(data) {
+            renderOne(agentId, data);
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            displayFormError(form, jqXHR.responseJSON);
+        });
     });
 });
