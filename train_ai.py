a84d373efe652e19778ba6daf441f4eb5f1a661f
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-11-17 18:15:40 +0100

    Reduce network complexity as it does not impact loss

--- train_ai.py
+++ train_ai.py
@@ -7,9 +7,9 @@ import simudyne
 
 
 if __name__ == '__main__':
-    D_IN, D_H1, D_H2, D_H3 = 9, 256, 128, 96
+    D_IN, D_H1 = 9, 32
     LEARNING_RATE = 1e-4
-    BATCH_SIZE = 10
+    BATCH_SIZE = 128
 
     print('Loading datasets...')
     train, valid, test = simudyne.ai.get_datasets(BATCH_SIZE, None)
@@ -17,11 +17,7 @@ if __name__ == '__main__':
     model = simudyne.ai.Model(
         nn.Linear(D_IN, D_H1),
         nn.ReLU(),
-        nn.Linear(D_H1, D_H2),
-        nn.ReLU(),
-        nn.Linear(D_H2, D_H3),
-        nn.ReLU(),
-        nn.Linear(D_H3, 1),
+        nn.Linear(D_H1, 1),
         nn.Sigmoid()
     )
 
