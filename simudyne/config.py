444085e1ee4a6bad03e49ce28cdf510aa7130bd8
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-11-17 20:50:52 +0100

    Move AI scripts to tools folder

--- simudyne/config.py
+++ simudyne/config.py
@@ -1,7 +1,11 @@
+import os
+
+ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+
 N_SIMULATED_YEARS = 15
 MIN_BRAND_FACTOR, MAX_BRAND_FACTOR = 0.1, 2.9
-DB_PATH = 'data/database.db'
-MODEL_PATH = 'data/model.pth'
-TRAINLOADER_PATH = 'data/trainloader.pickle'
-TESTLOADER_PATH = 'data/testloader.pickle'
-VALIDLOADER_PATH = 'data/validloader.pickle'
+DB_PATH = os.path.join(ROOT, 'data/database.db')
+MODEL_PATH = os.path.join(ROOT, 'data/model.pth')
+TRAINLOADER_PATH = os.path.join(ROOT, 'data/trainloader.pickle')
+TESTLOADER_PATH = os.path.join(ROOT, 'data/testloader.pickle')
+VALIDLOADER_PATH = os.path.join(ROOT, 'data/validloader.pickle')
